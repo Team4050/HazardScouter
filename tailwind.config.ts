499553ca2825b55297eb502231d035ad58070420
Carson Seese <carson@carsonseese.com>
2024-01-27 09:03:27 -0500

    boilerplate

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#16A34A",
+        background_color: "#4B5563",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "HazardScouter",
+        description: "FRC Team Biohazard 4050 scouting app",
+        name: "HazardScouter",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
