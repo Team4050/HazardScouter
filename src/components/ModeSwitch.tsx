f1ada27fb74b0c495d77812e4f8ba4d24700aa4f
Carson Seese <carson@carsonseese.com>
2024-03-05 20:16:11 -0500

    Disable mode switch for now

--- src/components/ModeSwitch.tsx
+++ src/components/ModeSwitch.tsx
@@ -1,30 +1,27 @@
-import { CameraIcon, TableCellsIcon } from "@heroicons/react/20/solid";
-import { Switch } from "@nextui-org/react";
-import { useLocation, useNavigate } from "react-router-dom";
-
 export default function ModeSwitch(): JSX.Element {
-  const location = useLocation();
-  const navigate = useNavigate();
+  // const location = useLocation();
+  // const navigate = useNavigate();
 
-  return (
-    <Switch
-      defaultSelected
-      size="lg"
-      startContent={<TableCellsIcon />}
-      endContent={<CameraIcon />}
-      isSelected={!location.pathname.includes("scanning")}
-      classNames={{
-        wrapper: "bg-primary",
-        startContent: "text-white",
-        endContent: "text-white",
-      }}
-      onClick={() => {
-        if (location.pathname.includes("scouting")) {
-          navigate("/scanning");
-        } else {
-          navigate("/scouting");
-        }
-      }}
-    />
-  );
+  // return (
+  //   <Switch
+  //     defaultSelected
+  //     size="lg"
+  //     startContent={<TableCellsIcon />}
+  //     endContent={<CameraIcon />}
+  //     isSelected={!location.pathname.includes("scanning")}
+  //     classNames={{
+  //       wrapper: "bg-primary",
+  //       startContent: "text-white",
+  //       endContent: "text-white",
+  //     }}
+  //     onClick={() => {
+  //       if (location.pathname.includes("scouting")) {
+  //         navigate("/scanning");
+  //       } else {
+  //         navigate("/scouting");
+  //       }
+  //     }}
+  //   />
+  // );
+  return <span>TODO</span>;
 }
