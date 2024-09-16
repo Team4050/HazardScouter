612e8d15fd697168cdb5ca56574bb9beb4d3ce6a
Carson Seese <carson@carsonseese.com>
2024-09-16 08:14:43 -0400

    Clean slate

--- src/pages/scouting.index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/scouting/")({
-  component: () => null,
-  loader: () => {
-    redirect({ to: "/scouting/$formPage", params: { formPage: "pre-match" } });
-  },
-});
