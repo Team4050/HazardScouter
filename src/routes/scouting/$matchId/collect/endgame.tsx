101c1fac78b2a22b7606a3526317e414506e192b
Carson Seese <carson@carsonseese.com>
2024-12-07 12:46:47 -0500

    Fix unsaved file

--- src/routes/scouting/$matchId/collect/endgame.tsx
+++ src/routes/scouting/$matchId/collect/endgame.tsx
@@ -1,9 +1,5 @@
-import { Select, Switch } from "@/components/inputs";
-import { matchCollection } from "@/data/db";
-import { EndStatus, endGameDefaults, endGameSchema } from "@/data/match/2024";
+import { Endgame } from "@/components/form/Endgame";
 import { useAppState } from "@/data/state";
-import { useForm } from "@/hooks/useForm";
-import { enumToSelectItem } from "@/util";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/scouting/$matchId/collect/endgame")({
@@ -15,27 +11,5 @@ export const Route = createFileRoute("/scouting/$matchId/collect/endgame")({
 
 function Page(): JSX.Element {
   const { matchId } = Route.useParams();
-  const form = useForm<"endgame">({
-    matchId,
-    phase: "endgame",
-    initialValues:
-      matchCollection.findOne({ id: matchId })?.phases.endgame ||
-      endGameDefaults,
-    schema: endGameSchema,
-  });
-
-  return (
-    <div className="grid gap-x-4 gap-y-2">
-      <Select
-        className="w-fit mx-auto"
-        label="End Status"
-        data={enumToSelectItem(EndStatus)}
-        {...form.getInputProps("endStatus")}
-      />
-      <Switch
-        label="Trap"
-        {...form.getInputProps("trap", { type: "checkbox" })}
-      />
-    </div>
-  );
+  return <Endgame matchId={matchId} />;
 }
