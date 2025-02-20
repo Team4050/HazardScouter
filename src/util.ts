442314fc078aec6155fdc0438ca2481d7829cd0b
Carson Seese <carson@carsonseese.com>
2025-02-20 08:28:16 -0500

    Fix title case function

--- src/util.ts
+++ src/util.ts
@@ -11,15 +11,17 @@ export type Override<
 
 export type Icon = FC<Omit<IconProps, "ref">>;
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
 
-export function titleCase(str: string) {
-  return str.replace(
-    /\w\S*/g,
-    (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase(),
-  );
+function titleCase(str: string): string {
+  return str
+    .replace(/-/g, " ")
+    .replace(
+      /\w\S*/g,
+      (text) => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase(),
+    );
 }
 
 export function enumToSelectItem<T extends string>(
